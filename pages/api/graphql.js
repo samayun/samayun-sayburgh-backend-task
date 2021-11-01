08e7e03af70d0dbe24fe70a6bd21481ef0323bb1
Samayun Miah Chowdhury <samayun.m.chowdhury@gmail.com>
2021-11-01 16:20:35 +0600

    Comment remove

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,20 +1,12 @@
 const { graphqlHTTP } = require("express-graphql");
-const model = require("../../graphql/model/index");
-const controllers = require("../../graphql/controller/index");
-import authenticateToken from '../../graphql/middleware/isAuth';
+const schema = require("../../graphql/model/index");
+const rootValue = require("../../graphql/controller/index");
+import authenticateToken from "../../graphql/middleware/isAuth";
 
 const GraphQL = graphqlHTTP({
-    schema: model,
-    rootValue: controllers,
-    graphiql: true,
-    customFormatErrorFn(error) {
-        return {
-            ...error,
-            // message: error.message,
-            // name: error.name || "An error occured",
-            // code: error.originalError ? error.originalError.code : 500
-        }
-    }
+  schema,
+  rootValue,
+  graphiql: true,
 });
 
 export default authenticateToken(GraphQL);
